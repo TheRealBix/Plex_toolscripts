d53d8369bfce0d2e59d18c5861e9f4003bd0a114
Bix <the.real.bix@gmail.com>
2026-05-06 22:01:57 +0200

    Add files via upload

---
+++ audio-delay.lua
@@ -0,0 +1,9 @@
+--Audio-delay
+--Sets a custom audio delay
+function setAudioDelay()
+    local delay = 0.24 -- Adjust this value as needed
+    mp.set_property("audio-delay", delay)
+    mp.msg.info("Audio delay set to " .. delay .. " seconds")
+end
+
+mp.register_event("file-loaded", setAudioDelay)
